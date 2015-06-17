ac1608ea68b76513763ec55dfc1f0eb71b1dbbce
Phelip Roberto <pheliproberto11@gmail.com>
2015-06-17 20:08:07 -0300

    pacote android

--- TechCar/app/src/main/java/unifei/edu/br/techcar/LoginActivity.java
+++ TechCar/app/src/main/java/unifei/edu/br/techcar/LoginActivity.java
@@ -1,15 +1,19 @@
 package unifei.edu.br.techcar;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends ActionBarActivity {
@@ -73,6 +77,32 @@ public class LoginActivity extends ActionBarActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // Equipe
+        if(id == R.id.action_team){
+            Intent team = new Intent();
+            team.setClass(LoginActivity.this,EquipeActivity.class);
+            startActivity(team);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     // Alterar status do login
     void preferencesChange(int status, String login){
